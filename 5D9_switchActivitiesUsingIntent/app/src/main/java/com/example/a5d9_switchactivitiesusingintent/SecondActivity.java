4f04bfed2e7547643c89306b3293c144413d1b55
harsha vardhan <harshabambu@gmail.com>
2025-02-07 11:45:15 +0530

    in experiment 5 second activity and url completed

---
+++ 5D9_switchActivitiesUsingIntent/app/src/main/java/com/example/a5d9_switchactivitiesusingintent/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.a5d9_switchactivitiesusingintent;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import android.content.Intent;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void show_main(View v){
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+}
